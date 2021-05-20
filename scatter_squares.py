b2457a3747462ddc88c29d45d1f1d92acf37482d
Cjhightower38 <Cjhightower38@gmail.com>
2021-05-19 21:45:00 -0400

    add label type and graph thickness

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,6 +1,16 @@
 # Using the scatter()
 import matplotlib.pyplot as plt
 
-plt.scatter(2, 4)
+# S = set the size of the dot.
+plt.scatter(2, 4, s = 200)
+
+# Set chart title and label axes.
+plt.title('Square Numbers', fontsize = 24)
+plt.xlabel('Value', fontsize = 14)
+plt.ylabel('Square of Value', fontsize = 14)
+
+# Set size of tick labels.
+plt.tick_params(axis = 'both', which = 'major', labelsize = 14)
+
 plt.show()
 
