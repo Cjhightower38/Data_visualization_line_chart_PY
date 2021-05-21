3d9b8de15ffb647926c9e76d402fa3658643d80e
Cjhightower38 <Cjhightower38@gmail.com>
2021-05-20 21:00:02 -0400

    automatically calculating the data.

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,12 +1,16 @@
 # Using the scatter()
 import matplotlib.pyplot as plt
 
-# X_values are the numbers to be squared and Y_values are the squares.
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+'''
+X_values are a list between 1 and 1000(off by 1) and Y_values are each 
+x in x_values squared.
+'''
+
+x_values = list(range(1, 1001))
+y_values = [x**2 for x in x_values]
 
 # S = set the size of the dot.
-plt.scatter(x_values, y_values, s = 100)
+plt.scatter(x_values, y_values, s = 40)
 
 # Set chart title and label axes.
 plt.title('Square Numbers', fontsize = 24)
@@ -15,6 +19,14 @@ plt.ylabel('Square of Value', fontsize = 14)
 
 # Set size of tick labels.
 plt.tick_params(axis = 'both', which = 'major', labelsize = 14)
+
+# Set the range for each axis.
+'''
+Axis() has 4 values the min and max for each the x_values and the 
+y_values.
+'''
+
+plt.axis([0, 1100, 0, 1100000])
 
 plt.show()
 
